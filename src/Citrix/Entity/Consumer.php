ad92a8184bf433419233259b07f1605adf13ed64
Jed Lynch <JethroT83@users.noreply.github.com>
2017-02-09 09:36:17 -0500

    Add files via upload

--- src/Citrix/Entity/Consumer.php
+++ src/Citrix/Entity/Consumer.php
@@ -87,11 +87,7 @@ class Consumer extends EntityAbstract implements EntityAware
   public function populate()
   {
     $data = $this->getData();
-    
-    $this->firstName = $data['firstName'];
-    $this->lastName = $data['lastName'];
-    $this->email = $data['email'];
-    
+
     if (isset($data['registrantKey'])) {
       $this->id = $data['registrantKey'];
     }
